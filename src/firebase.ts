326c5700f054235dac3ad262a728b341240c60ba
Roger Ojar <rogerojartt@gmail.com>
2022-07-11 11:57:49 -0400

    PWA ionic app

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCvKLr0QT2k-rXCigpBurdOO0s5FPUVSUI",
+  authDomain: "conf-pwa-2583b.firebaseapp.com",
+  projectId: "conf-pwa-2583b",
+  storageBucket: "conf-pwa-2583b.appspot.com",
+  messagingSenderId: "882667725258",
+  appId: "1:882667725258:web:77b9f60b8741047e5f2f33"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
